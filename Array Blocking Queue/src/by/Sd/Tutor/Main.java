11df9f9b26641aa250e1cfa73347f9897ae22343
Shadesher <Shadesher@192.168.1.134>
2017-08-07 18:51:40 +0600

    .

---
+++ Array Blocking Queue/src/by/Sd/Tutor/Main.java
@@ -0,0 +1,28 @@
+package by.Sd.Tutor;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Main {
+	private static ArrayBlockingQueue<Object> list = new ArrayBlockingQueue<Object>(1000);
+
+	public static void main(String[] args) {
+		
+		addObjects();
+		proceed();
+
+	}
+	private static void proceed() {
+		System.out.println(list.size());
+		for(Object element: list) {
+			list.remove(element);
+		}
+		System.out.println(list.size());
+	}
+
+	private static void addObjects() {
+		for (int x = 0; x < 5; x++) {
+			list.add(new Object());
+		}
+	}
+
+}
