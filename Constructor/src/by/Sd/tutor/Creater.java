437fe7b9a16019e29a0447adaa12c86e312a8629
Shadesher <Shadesher@192.168.1.134>
2017-08-09 16:40:18 +0600

    I Re-created a code

---
+++ Constructor/src/by/Sd/tutor/Creater.java
@@ -0,0 +1,20 @@
+package by.Sd.tutor;
+
+public class Creater {
+	private String name;
+	private boolean canFly;
+	
+	public Creater(String name, boolean canfly) {
+		this.name = name;
+		this.canFly = canfly;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean isCanFly() {
+		return canFly;
+	}
+
+}
---
+++ Constructor/src/by/Sd/tutor/Main.java
@@ -0,0 +1,11 @@
+package by.Sd.tutor;
+
+public class Main {
+	public static void main(String[] args) {
+		Creater create = new Creater("Ïòèöêà",true);
+		
+		System.out.println(create.getName());
+		System.out.println(create.isCanFly());
+	}
+
+}
