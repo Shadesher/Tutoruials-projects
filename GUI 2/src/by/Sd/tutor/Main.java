3c79120b24b4f0788a5ebb0b30137174f6c7186d
Shadesher <Shadesher@192.168.1.134>
2017-08-12 15:49:21 +0600

    .

---
+++ GUI 2/src/by/Sd/tutor/Main.java
@@ -0,0 +1,12 @@
+package by.Sd.tutor;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MainFrame frame = new MainFrame();
+		}
+
+}
---
+++ GUI 2/src/by/Sd/tutor/MainFrame.java
@@ -0,0 +1,19 @@
+package by.Sd.tutor;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class MainFrame extends JFrame {
+
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		setSize(800, 600);
+		getContentPane().setLayout(null);
+	}
+}
